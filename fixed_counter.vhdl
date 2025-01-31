08532ce016e8b08cc23677c0b48827782f8c757d
BugHunter-X <askzenai@gmail.com>
2025-01-31 17:46:07 +0100

    Add fixed_counter.vhdl file

---
+++ fixed_counter.vhdl
@@ -0,0 +1,30 @@
+```vhdl
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity fixed_counter is
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        count : out integer range 0 to 15
+    );
+end entity fixed_counter;
+
+architecture behavioral of fixed_counter is
+    signal internal_count : integer range 0 to 15 := 0;
+begin
+    process (clk, rst)
+    begin
+        if rst = '1' then
+            internal_count <= 0;
+        elsif rising_edge(clk) then
+            if internal_count = 15 then
+                internal_count <= 0;
+            else
+                internal_count <= internal_count + 1;  -- The else is implicitly here now due to the else if clause
+            end if;
+        end if;
+    end process;
+    count <= internal_count;
+end architecture behavioral;
+```
